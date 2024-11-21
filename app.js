ac38a318040bf3b31700da30f9a5e68dd0b74d29
Isaiah <Isaiahcwhite25@gmail.com>
2024-11-21 12:34:05 -0500

    Fourth-tri-Services-2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let isModalOpen = false
+
 var navLinks = document.getElementById("navLinks");
 function showMenu(){
     navLinks.style.right = '0'
@@ -21,3 +23,40 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+function toggleModal() {
+    if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+  }
+
+  function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+  
+    // emailjs
+    //   .sendForm(
+    //     "service_m6xikb3",
+    //     "template_hrhbd0p",
+    //     event.target,
+    //     "ivoXo6g3vlqMugLVm"
+    //   )
+//       .then(() => {
+//         loading.classList.remove("modal__overlay--visible");
+//         success.classList += " modal__overlay--visible";
+//       })
+//       .catch(() => {
+//         loading.classList.remove("modal__overlay--visible");
+//         alert(
+//           "The email service is temporaraly unavailable. Please contact me directly at dexxcode1996@gmail.com"
+//         );
+//       });
+//   
+}
+  
